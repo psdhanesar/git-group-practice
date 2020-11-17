a771c52fc691eda5fa49a198d0ccccf147dc2fb0
Balkirat Singh <balkiratsingh@Balkirats-MacBook-Pro.local>
2020-11-16 21:16:36 -0500

    added details2

--- src/main/java/com/pardeep/App.java
+++ src/main/java/com/pardeep/App.java
@@ -5,9 +5,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("The application is running");
+        System.out.println("The application");
 
         Customers customers = new Customers("amrit", 1);
         System.out.println(customers);
+    }
+
+    public void printDetails2() {
+        System.out.println("Details");
     }
 }
