ae5968763100fd4cc4d9c5512b252bb10da84fad
kaulmohit17 <mohit@fullstacklabs.ca>
2020-11-16 20:37:28 -0500

    added service

---
+++ src/main/java/com/pardeep/Service.java
@@ -0,0 +1,14 @@
+package com.pardeep;
+
+public class Service {
+    public boolean addAccount() {
+        System.out.println("The Account has been added");
+        return true;
+    }
+
+    public boolean isActive() {
+        System.out.println("The account is inactive");
+        return false;
+    }
+
+}
